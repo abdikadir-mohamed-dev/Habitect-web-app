c81321ed904c0118f4f7aa86c713931543cb3c16
abdikadir-mohamed-dev <abdikadirmohammed180@gmail.com>
2026-07-11 07:38:42 +0300

    the edited code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Properties from "./pages/Properties";
-import PropertyDetails from "./pages/PropertyDetails";
-import SavedProperties from "./pages/SavedProperties";
-import NotFound from "./pages/NotFound";
-
-export default function App() {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/properties" element={<Properties />} />
-          <Route path="/properties/:id" element={<PropertyDetails />} />
-          <Route path="/saved-properties" element={<SavedProperties />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
-  );
-}
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
@@ -32,38 +6,38 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+
 import AdminProperties from "./pages/AdminProperties";
 import AddProperty from "./pages/AddProperty";
 import EditProperty from "./pages/EditProperty";
 import ManageUsers from "./pages/ManageUsers";
 import Dashboard from "./pages/Dashboard";
 
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
 
+        {/* Public Routes */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
-          </Route>
+        </Route>
 
-          <Route path="/admin/properties" element={<AdminProperties />} />
+        {/* Admin Routes */}
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/properties" element={<AdminProperties />} />
+        <Route path="/admin/properties/new" element={<AddProperty />} />
+        <Route path="/admin/properties/edit/:id" element={<EditProperty />} />
+        <Route path="/admin/users" element={<ManageUsers />} />
 
-          <Route path="/admin/properties/new" element={<AddProperty />} />
-
-          <Route path="/admin/properties/edit/:id" element={<EditProperty />} />
-
-          <Route path="/admin/users" element={<ManageUsers />} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/properties/update" element={<EditProperty />} />
-
+        {/* 404 */}
         <Route path="*" element={<NotFound />} />
 
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
