9445261507462b1243516ed295cfd87930ed2b9a
Martin Waraho Mundia <martinwaraho2@gmail.com>
2026-07-20 17:59:38 +0300

    models.py code

---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# initializing SQLAlchemy (turns python classes to SQL databasse tables).
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    # helper line => links appointments directly to the user who booked them.
+    appointments = db.relationship('Appointment', backref='user', lazy=True)
+
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    property_title = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), default='Pending Approval')
