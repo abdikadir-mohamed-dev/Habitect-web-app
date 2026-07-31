b9a710f55868334860ebe821f94d4629a7138c67
abdikadir-mohamed-dev <abdikadirmohammed180@gmail.com>
2026-07-31 07:12:40 +0300

    updated user.py

---
+++ server/models/user.py
@@ -0,0 +1,46 @@
+from extensions import db, bcrypt
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=True
+    )
+
+    email = db.Column(
+        db.String(120),
+        nullable=False,
+        unique=True
+    )
+
+    password_hash = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+    role = db.Column(
+        db.String(20),
+        default="user"
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(
+            password
+        ).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(
+            self.password_hash,
+            password
+        )
